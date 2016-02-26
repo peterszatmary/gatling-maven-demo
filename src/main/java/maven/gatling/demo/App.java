e1ef40c6794c90af5c215e3951a89dfcc47bda1e
nue <peter.szatmary@gmail.com>
2016-02-26 19:12:26 +0100

    Preparation commit.

--- src/main/java/maven/gatling/demo/App.java
+++ src/main/java/maven/gatling/demo/App.java
@@ -4,10 +4,9 @@ package maven.gatling.demo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
